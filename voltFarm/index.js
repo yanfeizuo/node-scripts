0711be48b5a7f8f6ba8906293fc2348159afcf87
yanfeizuo <18856856693@163.com>
2024-12-11 08:13:47 +0000

    udpate voltFarm script

--- voltFarm/index.js
+++ voltFarm/index.js
@@ -1,26 +1,26 @@
 const { ethers } = require("ethers")
-const { ERC20_ABI, VaultTemplateABI } = require("../ABI")
+const { ERC20_ABI, VaultTemplateABI } = require("../ABI");
+const { default: BigNumber } = require("bignumber.js");
 
 const thetaRpc = "https://eth-rpc-api.thetatoken.org/rpc"
 
 const tfuel_busd = "0x743e7034652b7555Ed96c280Ae89f5EC0383696f"
 
-const users = [
+// 0xE7eCE3aF2F8100B2e6844D5Be8a07eCCe8D4864a
+
+const geysers = [
   {
-    wallet: "0xE7eCE3aF2F8100B2e6844D5Be8a07eCCe8D4864a",
-    geyser: {
-      "id": "0x743e7034652b7555ed96c280ae89f5ec0383696f",
-      "rewardToken": "0xe6a991ffa8cfe62b0bf6bf72959a3d4f11b2e0f5",
-      "stakingToken": "0xf5819ccc999057618f6bb1102aa2913eaeda8d6c",
-    }
+    "id": "0x2A9a2843BA7C5dBc70592377e60aACD7EE27D0D9",
+    "rewardToken": "0xe6a991ffa8cfe62b0bf6bf72959a3d4f11b2e0f5",
+    "stakingToken": "0xf5819ccc999057618f6bb1102aa2913eaeda8d6c",
   }
 ]
 
 ;(async () => {
   const jsonRpcProvider = new ethers.providers.JsonRpcProvider(thetaRpc)
-  for (const u of users) {
-    console.log('u', u)
-    const { id, rewardToken, stakingToken } = u.geyser
+  for (const geyser of geysers) {
+    console.log('geyser', geyser)
+    const { id, rewardToken, stakingToken } = geyser
     const rewardTokenContract = new ethers.Contract(rewardToken, ERC20_ABI, jsonRpcProvider)
     const stakingTokenContract = new ethers.Contract(stakingToken, ERC20_ABI, jsonRpcProvider)
     
@@ -29,7 +29,7 @@ const users = [
     // user reward token
     // balanceOf
     const rewardBalance = await rewardTokenContract.balanceOf(id)
-    console.log('rewardBalance', rewardBalance)
+    console.log('rewardBalance', BigNumber(String(rewardBalance)).div(1e18).toFixed())
 
     // locked balance
     const rewardLockedBalance = await vaultContract.getBalanceLocked(rewardToken)
@@ -38,7 +38,7 @@ const users = [
     // user staking token
     // balanceOf
     const stakingBalance = await stakingTokenContract.balanceOf(id)
-    console.log('stakingBalance', stakingBalance)
+    console.log('stakingBalance', BigNumber(String(stakingBalance)).div(1e18).toFixed())
 
     // locked balance
     const stakingLockedBalance = await vaultContract.getBalanceLocked(stakingToken)
