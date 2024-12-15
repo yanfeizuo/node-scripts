f6998cf776d656e80013d9f24bdb5e5940c90b8a
yanfeizuo <18856856693@163.com>
2024-12-15 11:19:37 +0000

    update pell staking test

--- pellStaking/index.js
+++ pellStaking/index.js
@@ -8,7 +8,7 @@ const { MulticallWrapper } = require("ethers-multicall-provider")
 const coreRpc = "https://rpc.coredao.org"
 const PellStakingContract = "0x57bF5B3492Fef24A4f883135CB2AAD27Ce227183"
 
-const getCoreDaoPellStakingSuBTC = async (accounts) => {
+const getCoreDaoPellStakingSuBTC = async (accounts, price) => {
   const pell = new ethers.Contract(PellStakingContract, PellStakingABI, MulticallWrapper.wrap(new ethers.JsonRpcProvider(coreRpc)))
   // get shares
   const getSharesCallMap = accounts.map(acc => pell.shares(acc))
@@ -16,28 +16,36 @@ const getCoreDaoPellStakingSuBTC = async (accounts) => {
   console.log('getSharesRes', getSharesRes)
 
   const allShares = getSharesRes.reduce((acc, cur) => acc.plus(cur), new BigNumber(0))
-  if (allShares.isZero(0)) {
-    return BigNumber(0)
-  }
+  console.log('allShares', allShares)
+  // if (allShares.isZero(0)) {
+  //   return BigNumber(0)
+  // }
 
   const sharesToUnderlyingViewCallMap = []
   getSharesRes.map(s => {
-    if (s > 0) {
+    // if (s > 0) {
       sharesToUnderlyingViewCallMap.push(pell.sharesToUnderlyingView(s))
-    }
+    // }
   })
   
   const sharesToUnderlyingViewCallRes = await Promise.all(sharesToUnderlyingViewCallMap)
+  console.log('sharesToUnderlyingViewCallRes', sharesToUnderlyingViewCallRes)
   const totalUnderlying = sharesToUnderlyingViewCallRes.reduce((acc, cur) => acc.plus(cur), new BigNumber(0))
+  console.log('totalUnderlying', totalUnderlying)
 
+  return totalUnderlying.div(1e18).times(price)
+}
+
+const getBitcoinPrice = async () => {
   const priceData = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
   const btcPrice = priceData.data?.bitcoin?.usd || 97165
-
-  return totalUnderlying.div(1e18).times(btcPrice)
+  return btcPrice
 }
 
 ;(async () => {
-  const accounts = ["0x57e7e16A2326DC41d02402103A73b4464A8B3EEb"]
-  const val = await getCoreDaoPellStakingSuBTC(accounts)
-  console.log('val', val)
+  const accounts = ["0x57e7e16A2326DC41d02402103A73b4464A8B3EEb", "0xd5C20Eb8345F71717c367075c45a3908a0fC1Ce8"]
+  const bitcoinPrice = await getBitcoinPrice()
+  console.log('bitcoinPrice', bitcoinPrice)
+  const val = await getCoreDaoPellStakingSuBTC(accounts, bitcoinPrice)
+  console.log('val', val.toFixed())
 })()
