5d1917727f3bdde10ff616800ba802297588f33c
yanfeizuo <18856856693@163.com>
2025-01-09 05:43:58 +0000

    update

--- layerzerov2/index.js
+++ layerzerov2/index.js
@@ -28,11 +28,11 @@ const layerzerov2Config = {
     lzEndpoint: "0x1a44076050125825900e736c501f859c50fE728c",
   },
   arbitrumsepolia: {
-    lzEndpointId: 421614,
+    lzEndpointId: 40231,
     lzEndpoint: "0x6EDCE65403992e310A62460808c4b910D972f10f",
   },
   basesepolia: {
-    lzEndpointId: 84532,
+    lzEndpointId: 40245,
     lzEndpoint: "0x6EDCE65403992e310A62460808c4b910D972f10f",
   },
   sepolia: {
@@ -55,37 +55,53 @@ const layerzerov2Config = {
     lzEndpointId: 30153,
     lzEndpoint: "0x1a44076050125825900e736c501f859c50fE728c",
   },
+  zksyncsepolia: {
+    lzEndpointId: 40305,
+    lzEndpoint: "0xe2Ef622A13e71D9Dd2BBd12cd4b27e1516FA8a09",
+  },
+  zklinksepolia: {
+    lzEndpointId: 40283,
+    lzEndpoint: "0xf3e37ca248ff739b8d0bebcceae1eeb199223dba",
+  },
 }
 
 // metertest
-const proxyAddr = "0xB41a27CEa6535C8D972607f9Bc81590C409Ab8f2"
-const rpc = "https://rpctest.meter.io"
-const tokenAddress = "0x96159A91291c92dF19983E53E14726b1de3f7c49"  // metertest suusd
+// const proxyAddr = "0xB41a27CEa6535C8D972607f9Bc81590C409Ab8f2"
+// const rpc = "https://rpctest.meter.io"
+// const tokenAddress = "0x96159A91291c92dF19983E53E14726b1de3f7c49"  // metertest suusd
 
 // sepolia
-// const proxyAddr = "0xeEA94706C3F4760c6EED7590a53D8F394685817a"
-// const rpc = "https://eth-sepolia.public.blastapi.io"
-// const tokenAddress = "0x7215b1853E846dBB6B4F5639563be5E2f1A9489f"  // sepolia suusd
+const proxyAddr = "0xeEA94706C3F4760c6EED7590a53D8F394685817a"
+const rpc = "https://eth-sepolia.public.blastapi.io"
+const tokenAddress = "0x7215b1853E846dBB6B4F5639563be5E2f1A9489f"  // sepolia suusd
 
 // arbsepolia
 // const proxyAddr = "0xC9B4601178FbEd19210E0CC0EA657dF235B88768"
 // const rpc = "https://arbitrum-sepolia-rpc.publicnode.com"
-// const tokenAddress = "0x8DBA7Bf9dB7f6b2Cbe7c2CdABe2dF25756E6B5a3"  // sepolia suusd
+// const tokenAddress = "0x8DBA7Bf9dB7f6b2Cbe7c2CdABe2dF25756E6B5a3"  // arbsepolia suusd
 
 // basesepolia
 // const proxyAddr = "0x092fa73f599f3CcEa2093d65c9fDcb4A49fa7924"
 // const rpc = "https://sepolia.base.org"
-// const tokenAddress = "0x3b072FEA46117ac64F7B5baa0E60AfF4F966001b"  // sepolia suusd
+// const tokenAddress = "0x3b072FEA46117ac64F7B5baa0E60AfF4F966001b"  // basesepolia suusd
 
-const amount = BigInt(1e18)
-const toAddress = "0x57e7e16A2326DC41d02402103A73b4464A8B3EEb"
+// zksyncsepolia
+// const proxyAddr = "0xE556e5C72a1338cbe6b3906eb94172bA43c7e120"
+// const rpc = "https://sepolia.era.zksync.dev"
+// const tokenAddress = ""
 
-const dEid = layerzerov2Config.basesepolia.lzEndpointId
+const amount = BigInt(5e18)
+// const toAddress = "0x57e7e16A2326DC41d02402103A73b4464A8B3EEb"
+// const toAddress = "0x20255b38ece64adff5184a29e5a9a680b88a58c6"
+
+const dEid = layerzerov2Config.arbitrumsepolia.lzEndpointId
 
 const provider = new ethers.JsonRpcProvider(rpc)
 
 const send = async (key) => {
   const wallet = new ethers.Wallet(key, provider)
+  const toAddr = await wallet.getAddress()
+  console.log('toAddr', toAddr)
   const proxy = new ethers.Contract(proxyAddr, LayerZeroV2ABI, wallet)
 
   const GAS_LIMIT = 1000000; // Gas limit for the executor
@@ -98,7 +114,7 @@ const send = async (key) => {
 
   const sendParam = {
     dstEid: dEid,
-    to: zeroPadValue(toAddress, 32),
+    to: zeroPadValue(toAddr, 32),
     token: tokenAddress,
     amountLD: amount,
     minAmountLD: amount,
@@ -114,47 +130,29 @@ const send = async (key) => {
   console.log('_shouldEnterTimelock', _shouldEnterTimelock)
 
   const token = new ethers.Contract(tokenAddress, ERC20_ABI, wallet)
-  const allowance = await token.allowance(toAddress, proxyAddr)
+  const allowance = await token.allowance(toAddr, proxyAddr)
   console.log('allowance', allowance)
   if (allowance < amount) {
     const approveTx = await token.approve(proxyAddr, amount)
     await approveTx.wait()
   }
 
-  const myTokenBalance = await token.balanceOf(toAddress)
+  const myTokenBalance = await token.balanceOf(toAddr)
   console.log('myTokenBalance', myTokenBalance)
 
-  const allLane = await proxy.getAllLane()
-  console.log('allLane', allLane.map(a => a.toObject()))
+  // const allLane = await proxy.getAllLane()
+  // console.log('allLane', allLane.map(a => a.toObject()))
   
 
   const tx = await proxy.send(
     sendParam,
     { nativeFee: fee.nativeFee, lzTokenFee: fee.lzTokenFee },
-    toAddress,
+    toAddr,
     _shouldEnterTimelock,
     { value: fee.nativeFee }
   );
   const receipt = await tx.wait();
   console.log(`sent ${receipt?.hash}`);
-}
-
-const TimelockAddrSepolia = "0x773b5cDf58eaE454ca72DCdBb9B957f0e5b709d8"
-
-const sepoliaRpc = "https://ethereum-sepolia-rpc.publicnode.com"
-const providerSepolia = new ethers.JsonRpcProvider(sepoliaRpc)
-const walletSepolia = new ethers.Wallet(TPK, providerSepolia)
-
-const timelock = async () => {
-  const sepoliaTimelock = new ethers.Contract(TimelockAddrSepolia, LayerZeroV2TimelockABI, walletSepolia)
-  const userAgreements = await sepoliaTimelock.userAgreements(toAddress)
-  console.log('userAgreements', userAgreements.map(a => a.toObject()))
-  const agreementsObj = userAgreements.map(a => a.toObject())
-  for (const [idx, agreement] of agreementsObj.entries()) {
-    console.log('agreement', agreement, idx)
-    const estimateReleaseTime = await sepoliaTimelock.getMinWaitInSeconds(toAddress, idx)
-    console.log('estimateReleaseTime', estimateReleaseTime)
-  }
 }
 
 ;( async () => {
@@ -163,5 +161,4 @@ const timelock = async () => {
   const privateKey = args[2] || TPK
   console.log('pri', privateKey)
   await send(privateKey)
-  // await timelock()
 })()
